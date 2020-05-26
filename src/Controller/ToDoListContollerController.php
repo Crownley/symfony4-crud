1813cd02bbf4d79121b0fca317d02aae5fdd8172
Edgaras Avagyan <35609247+Crownley@users.noreply.github.com>
2020-05-26 06:39:03 +0300

    CRUD finsihed

--- src/Controller/ToDoListContollerController.php
+++ src/Controller/ToDoListContollerController.php
@@ -2,19 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListContollerController extends AbstractController
 {
     /**
-     * @Route("/to/do/list/contoller", name="to_do_list_contoller")
+     * @Route("/", name="to_do_list")
      */
     public function index()
+
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ToDoListContollerController.php',
-        ]);
+        $tasks = $this->getDoctrine()->getRepository(TASK::class)->findBy([],['id'=>'DESC']);
+        return $this->render('index.html.twig', ['tasks'=>$tasks]);
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+        $title = trim($request->request->get('title'));
+        if(empty($title)) 
+        return $this->redirectToRoute('to_do_list');
+        
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
+
+    }
+
+     /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus( !$task->getStatus());
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
+    }
+
+     /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete(Task $id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($id);
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
     }
 }
